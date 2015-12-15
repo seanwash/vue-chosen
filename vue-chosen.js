b3b6e00942b1aedd72eef507489f560da877b6a1
Sean Washington <hello@seanwash.com>
2015-12-14 19:29:01 -0800

    Update vue-chosen.js

--- vue-chosen.js
+++ vue-chosen.js
@@ -1,23 +1,25 @@
 Vue.directive('chosen', {
   twoWay: true,
   bind: function() {
-    return $(this.el).chosen({
-      inherit_select_classes: false,
-      width: '100%'
-    }).change((function(_this) {
-      return function(ev) {
-        var i, len, option, ref, values;
-        if (_this.el.hasAttribute('multiple')) {
-          values = [];
-          ref = _this.el.selectedOptions;
-          for (i = 0, len = ref.length; i < len; i++) {
-            option = ref[i];
-            values.push(option.value);
+    return this.vm.$nextTick((function(_this) {
+      return function() {
+        return $(_this.el).chosen({
+          inherit_select_classes: false,
+          width: '100%'
+        }).change(function(ev) {
+          var i, len, option, ref, values;
+          if (_this.el.hasAttribute('multiple')) {
+            values = [];
+            ref = _this.el.selectedOptions;
+            for (i = 0, len = ref.length; i < len; i++) {
+              option = ref[i];
+              values.push(option.value);
+            }
+            return _this.set(values);
+          } else {
+            return _this.set(_this.el.value);
           }
-          return _this.set(values);
-        } else {
-          return _this.set(_this.el.value);
-        }
+        });
       };
     })(this));
   },
