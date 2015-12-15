1793c14442454a7d7108a0dca3a9335ee63e879a
Sean Washington <hello@seanwash.com>
2015-12-14 18:28:06 -0800

    Add js file

---
+++ vue-chosen.js
@@ -0,0 +1,27 @@
+Vue.directive('chosen', {
+  twoWay: true,
+  bind: function() {
+    return $(this.el).chosen({
+      inherit_select_classes: false,
+      width: '100%'
+    }).change((function(_this) {
+      return function(ev) {
+        var i, len, option, ref, values;
+        if (_this.el.hasAttribute('multiple')) {
+          values = [];
+          ref = _this.el.selectedOptions;
+          for (i = 0, len = ref.length; i < len; i++) {
+            option = ref[i];
+            values.push(option.value);
+          }
+          return _this.set(values);
+        } else {
+          return _this.set(_this.el.value);
+        }
+      };
+    })(this));
+  },
+  update: function(nv, ov) {
+    return $(this.el).trigger('chosen:updated');
+  }
+});
